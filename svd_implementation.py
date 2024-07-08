bcb0421b9523109c648fc59f2976fa715c5d4ad1
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2024-07-08 19:20:22 +0300

    svd comments and verify_svd()
    -added comments to svd_implementation.py
    -created separate func to check svd correctness

--- svd_implementation.py
+++ svd_implementation.py
@@ -1,41 +1,43 @@
 import numpy as np
-import random
 
-
-a = np.random.randint(0, 256, (lambda n=random.randint(1, 5): (n, n))())
-print(f"Matrix A:\n", a)
+from objects import matrix as a
 
 
 def custom_svd(a):
     aat = np.dot(a, a.T)
     ata = np.dot(a.T, a)
 
-    eigenvalues_u, u = np.linalg.eigh(aat)
+    eigenvalues_u, u = np.linalg.eigh(aat)  # eigenvectors are already orthonormal
     eigenvalues_v, v = np.linalg.eigh(ata)
 
-    sorted_indices_u = np.argsort(eigenvalues_u)[::-1]
+    sorted_indices_u = np.argsort(eigenvalues_u)[::-1]  # get and sort indices of eigenvalues in descending order
     sorted_indices_v = np.argsort(eigenvalues_v)[::-1]
 
-    eigenvalues_u = eigenvalues_u[sorted_indices_u]
+    eigenvalues_u = eigenvalues_u[sorted_indices_u]  # sort eigenvalues
 
-    u = u[:, sorted_indices_u]
+    u = u[:, sorted_indices_u]  # sort eigenvectors to get u and v
     v = v[:, sorted_indices_v]
 
-    sigma = np.zeros((u.shape[0], v.shape[0]), dtype=float)
+    sigma = np.zeros(a.shape)  # create m*n matrix sigma filled with 0
     min_dim = min(a.shape)
-    sigma[:min_dim, :min_dim] = np.diag(np.sqrt(eigenvalues_u[:min_dim]))
+    sigma[:min_dim, :min_dim] = np.diag(
+        np.sqrt(eigenvalues_u[:min_dim]))  # fill sigma's diagonal with min{m, n} singular values
 
     for i in range(min_dim):
-        u[:, i] = np.dot(a, v[:, i]) / sigma[i, i]
+        u[:, i] = np.dot(a, v[:, i]) / sigma[i, i]  # align left singular vectors U according to formula
 
-    a_reconstructed = np.dot(u, np.dot(sigma, v.T))
-
-    return u, sigma, v.T, a_reconstructed
+    return u, sigma, v.T
 
 
-u, sigma, v_T, a_reconstructed = custom_svd(a)
+def verify_svd(u, sigma, vt):
+    return np.allclose(a, np.dot(u, np.dot(sigma, vt)), rtol=1e-05, atol=1e-08)
 
-print("Matrix U:\n", u)
+
+u, sigma, vt = custom_svd(a)
+is_correct_svd = verify_svd(u, sigma, vt)
+
+print(f"Matrix A:\n", a)
+print("\nMatrix U:\n", u)
 print("Matrix Sigma:\n", sigma)
-print("Matrix V^T:\n", v_T)
-print("Reconstructed Matrix A:\n", a_reconstructed)
+print("Matrix V^T:\n", vt)
+print("\nSVD is correct:\n", is_correct_svd)
