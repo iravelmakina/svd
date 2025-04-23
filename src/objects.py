89f461743405c18e507c35d809d98be0f86f26a3
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2025-04-24 00:19:56 +0300

    update project structure

--- src/objects.py
+++ src/objects.py
@@ -3,5 +3,5 @@ import numpy as np
 
 
 matrix = np.random.randint(0, 256, (lambda n=random.randint(1, 5): (n, n))())
-file_path_ratings = 'ratings.csv'
-file_path_movies = 'movies.csv'
+file_path_ratings = '../data/ratings.csv'
+file_path_movies = '../data/movies.csv'
